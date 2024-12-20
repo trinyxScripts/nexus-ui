9051ba05abbf9977b5ff0e1fa618f4183e7677f1
trinyxScripts <thrinyxt@icloud.com>
2024-12-20 17:28:05 +0200

    Update nexuslib.lua

--- nexuslib.lua
+++ nexuslib.lua
@@ -2,6 +2,6 @@ print("Currently Under Fix")
 
 game:GetService("StarterGui"):SetCore("SendNotification",{
 	Title = "Not Working", -- Required
-	Text = "The UI Library is Under Fix Sorry", -- Required
+	Text = "The UI Library is Under Fixxing Sorry", -- Required
 	Icon = "rbxassetid://1234567890" -- Optional
 })
